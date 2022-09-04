9fe32791f84609bb3dfdf6d7fb042912b9064876
DevilxDude <ddude786@gmail.com>
2022-09-04 22:50:57 +0530

    Created default Hello World App on Get API endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+// Import Rocket
+#[macro_use]
+extern crate rocket;
+
+// Default get route
+#[get("/")]
+fn say_hello() -> &'static str {
+    "Hello, welcome to the api!"
+}
+
+// Starting Rocket Server
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/api", routes![say_hello])
 }
